839cfbfe96fb895cc68519209cdb466f30f4bbd2
elenapark <elenability@naver.com>
2022-10-13 12:19:42 +0900

    add : mongoDB, mongoose 세팅

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -8,8 +10,13 @@ const corsOptions = require("./config/corsOptions");
 const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
 const credentials = require("./middleware/credentials");
+const mongoose = require("mongoose");
+const connectDB = require("./config/dbConn");
 
 const PORT = process.env.PORT || 3500;
+
+// Connect to DB
+connectDB();
 
 // custom middleware logger
 app.use(logger);
@@ -65,4 +72,7 @@ app.all("/*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
