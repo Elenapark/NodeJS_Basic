9e3f4f669ceb3b167d3e7dd2c51dd7719ee41784
elenapark <elenability@naver.com>
2022-10-16 19:57:09 +0900

    fix: react프로젝트 연결 위한 allowed origin 추가, register 409응답 시 return문 제외 오류 수정

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["https://www.google.com", "http://localhost:3500"];
+const allowedOrigins = [
+  "https://www.google.com",
+  "http://localhost:3500",
+  "http://localhost:3000",
+];
 
 module.exports = allowedOrigins;
