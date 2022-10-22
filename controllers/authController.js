44b4b505029365583b0a7304ef3ca5e7dc52889b
elenapark <elenability@naver.com>
2022-10-23 01:12:05 +0900

    fix/remove: change token expiration time/remove roles in res

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,7 +29,7 @@ const handleLogin = async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "10s" }
+      { expiresIn: "10m" }
     );
     // only for getting new access_token
     const refreshToken = jwt.sign(
@@ -50,7 +50,7 @@ const handleLogin = async (req, res) => {
       secure: true,
       maxAge: 24 * 60 * 60 * 1000, // 1day
     });
-    res.json({ roles, accessToken });
+    res.json({ accessToken });
   } else {
     res.sendStatus(401);
   }
